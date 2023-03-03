69fe0f8109b71f7645a7a39f024b66ef0ae83ac9
dekinghtg <deking.htg@gmail.com>
2023-03-03 20:42:35 +0700

    function2

--- functions.js
+++ functions.js
@@ -1,11 +1,26 @@
 "use strict";
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 console.log(add(2, 3));
+console.log(add(2, 3, 4));
 const sub = (num1, num2) => num1 - num2;
 console.log(sub(10, 3));
 const mult = function (num1, num2) {
     return num1 * num2;
 };
 console.log(mult(7, 5));
+function add2(num1, num2, ...num3) {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+let numbers = [1, 2, 3, 4, 5];
+console.log(add2(2, 3, ...numbers));
+console.log(add2(2, 3, ...[2, 3, 4]));
+console.log(add2(2, 3, 4, 5, 6, 7, 8, 9));
+function getItems(items) {
+    return new Array().concat(items);
+}
+let concatResult = getItems([1, 2, 3, 4, 5]);
+let concatString = getItems(["a", "b", "c", "d", "e"]);
+console.log(concatResult);
+console.log(concatString);
--- functions.ts
+++ functions.ts
@@ -1,8 +1,10 @@
-function add(num1: number, num2: number): number {
-    return num1 + num2;
+function add(num1: number, num2: number, num3?: number): number {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
 
 console.log(add(2, 3));
+
+console.log(add(2, 3, 4));
 
 const sub = (num1: number, num2: number): number => num1 - num2;
 
@@ -12,4 +14,23 @@ const mult = function (num1: number, num2: number): number {
     return num1 * num2;
 }
 
-console.log(mult(7,5));
+console.log(mult(7, 5));
+
+function add2(num1: number, num2: number, ...num3: number[]): number {
+    return num1 + num2 + num3.reduce((a, b) => a + b, 0);
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(add2(2, 3, ...numbers));
+console.log(add2(2, 3, ...[2, 3, 4]));
+console.log(add2(2, 3, 4, 5, 6, 7, 8, 9));
+
+function getItems<Type>(items: Type[]): Type[] {
+    return new Array<Type>().concat(items);
+}
+
+let concatResult = getItems<number>([1,2,3,4,5]);
+let concatString = getItems<string>(["a","b","c","d","e"]);
+
+console.log(concatResult);
+console.log(concatString);
